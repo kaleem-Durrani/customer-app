2b2890ac9f656f797a3eaa60a2e1a09b65ffaa4e
kaleem-Durrani <kalimdurrani22@gmail.com>
2024-06-23 02:40:07 -0700

    QR Modal component created, QR generation functionality added to the purchase screen

--- Screens/BottomTabs/Purchase/Purchase.tsx
+++ Screens/BottomTabs/Purchase/Purchase.tsx
@@ -1,13 +1,34 @@
-import { View, HStack, VStack, Button, ButtonText } from "@gluestack-ui/themed";
 import React, { useState } from "react";
+import {
+  View,
+  HStack,
+  VStack,
+  Button,
+  ButtonText,
+  Text,
+  Center,
+} from "@gluestack-ui/themed";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { COLORS } from "../../../Constants/Constants";
 import ImageButton from "./components/ImageButton";
 import FuelTypeAmountCard from "./components/FuelTypeAmountCard";
 import MyToast from "../../../components/MyToast";
+import QRModal from "./components/QRModal";
+
+interface QRDictionary {
+  selectedFuel: string;
+  amount: string;
+  litres: string;
+  selectedPaymentMethod: string;
+}
 
 const Purchase = () => {
   const toast = MyToast();
   const [showingToast, setShowingToast] = useState(false);
+
+  // QR Modal control state
+  const [showModal, setShowModal] = useState(false);
+  const [QRDictionary, setQRDictionary] = useState<QRDictionary | null>(null);
 
   const [amount, setAmount] = useState("");
   const [litres, setLitres] = useState("");
@@ -16,51 +37,56 @@ const Purchase = () => {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("");
 
   const showErrorToast = (title: string, description: string) => {
+    if (showingToast) return;
+
     toast.show("accent", "error", title, description, 5000);
     setShowingToast(true);
-    setTimeout(() => {
-      setShowingToast(false);
-    }, 4500);
+    setTimeout(() => setShowingToast(false), 4500);
   };
 
   const onBuyPress = () => {
     if (!selectedFuel) {
-      if (showingToast) return;
       return showErrorToast(
         "Fuel Type Not Selected",
-        "Please select the type of fuel you widh to purchase"
+        "Please select the type of fuel you wish to purchase"
       );
     }
     if (!amount || !litres) {
-      if (showingToast) return;
       return showErrorToast(
         "Amount or Litres Not Selected",
         "Please select the amount for the fuel you want to buy or the number of litres"
       );
     }
     if (!selectedPaymentMethod) {
-      if (showingToast) return;
       return showErrorToast(
         "Payment Method Not Selected",
-        "Please select one of the four payment mehods at the bottom of the screen"
+        "Please select one of the four payment methods at the bottom of the screen"
       );
     }
+
     // all requirements fulfilled
     toast.show(
       "accent",
       "success",
       "All required Info Given",
-      `You wish to buy ${selectedFuel} of ${amount} amount and ${litres} litres your payment method is ${selectedPaymentMethod}`,
+      `You wish to buy ${selectedFuel} of ${amount} amount and ${litres} litres. Your payment method is ${selectedPaymentMethod}.`,
       5000
     );
+
+    const myDictionary: QRDictionary = {
+      selectedFuel,
+      amount,
+      litres,
+      selectedPaymentMethod,
+    };
+
+    setQRDictionary(myDictionary);
+    setShowModal(true);
   };
 
   return (
     <View flex={1} bg={COLORS.primary} pt={"$1"} px={"$4"}>
-      {/* 
-      the top card of the screen containing the fuel type selection
-      and form controls for the amount ano no of litres
-       */}
+      {/* The top card of the screen containing the fuel type selection and form controls for the amount and number of litres */}
       <FuelTypeAmountCard
         selectedFuel={selectedFuel}
         setSelectedFuel={setSelectedFuel}
@@ -106,7 +132,7 @@ const Purchase = () => {
           />
         </HStack>
 
-        {/* buy button */}
+        {/* Buy button */}
         <Button
           backgroundColor={COLORS.tertiary}
           mx={"$7"}
@@ -116,6 +142,11 @@ const Purchase = () => {
           <ButtonText>Buy Now!</ButtonText>
         </Button>
       </VStack>
+      <QRModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        QRDictionary={QRDictionary}
+      />
     </View>
   );
 };
---
+++ Screens/BottomTabs/Purchase/components/QRModal.tsx
@@ -0,0 +1,102 @@
+import {
+  View,
+  Text,
+  ButtonText,
+  Center,
+  Button,
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  ModalHeader,
+  Heading,
+  ModalCloseButton,
+  Icon,
+  CloseIcon,
+  ModalBody,
+  ModalFooter,
+  VStack,
+  HStack,
+} from "@gluestack-ui/themed";
+import React, { useState } from "react";
+import QRCode from "react-native-qrcode-svg";
+import { PERCENT } from "../../../../Constants/Constants";
+
+const QRModal = ({ showModal, setShowModal, QRDictionary }: any) => {
+  const generateQRString = (data: any) => {
+    const jsonString = JSON.stringify(data);
+    return jsonString;
+  };
+
+  const QRString = generateQRString(QRDictionary);
+
+  return (
+    <Center h={300}>
+      <Modal
+        isOpen={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+      >
+        <ModalBackdrop onPress={() => {}} />
+        <ModalContent>
+          <ModalHeader>
+            <Heading size="lg">Have your QR scanned!</Heading>
+            <ModalCloseButton>
+              <Icon as={CloseIcon} size="xl" />
+            </ModalCloseButton>
+          </ModalHeader>
+          <ModalBody>
+            <View alignSelf="center" borderWidth={1} p={"$3"}>
+              <QRCode value={QRString} size={PERCENT[50]} />
+            </View>
+          </ModalBody>
+          <ModalFooter>
+            {/* <Button
+              variant="outline"
+              size="sm"
+              action="secondary"
+              mr="$3"
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Cancel</ButtonText>
+            </Button> */}
+            <VStack flex={1}>
+              <HStack justifyContent="space-between">
+                <Text size="xs" color="gray">
+                  Fuel Type: {QRDictionary.selectedFuel}
+                </Text>
+                <Text size="xs" color="gray">
+                  Amount: {QRDictionary.amount}
+                </Text>
+              </HStack>
+              <HStack mb={"$2"} justifyContent="space-between">
+                <Text size="xs" color="gray">
+                  Litres:{QRDictionary.litres}
+                </Text>
+                <Text size="xs" color="gray">
+                  Payment Method: {QRDictionary.selectedPaymentMethod}
+                </Text>
+              </HStack>
+
+              <Button
+                alignSelf="flex-end"
+                size="sm"
+                action="negative"
+                borderWidth="$0"
+                onPress={() => {
+                  setShowModal(false);
+                }}
+              >
+                <ButtonText>Regenerate QR</ButtonText>
+              </Button>
+            </VStack>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Center>
+  );
+};
+
+export default QRModal;
